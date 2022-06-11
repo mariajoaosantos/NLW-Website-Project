ffdeaf0f8b8699fabebdc488e54c54700e4d710d
mariajoaosantos <mariajcs18@gmail.com>
2022-06-11 17:01:19 +0100

    Nav menu done

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 function onScroll() {
-  document.getElementById('navigation').classList.add('scroll')
+  if (scrollY == 0) {
+    document.getElementById('navigation').classList.remove('scroll')
+  } else {
+    document.getElementById('navigation').classList.add('scroll')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
 }
